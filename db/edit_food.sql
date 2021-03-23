3277f466998b57f703c2474520a51a3f44ca5304
Terrance Cazy <terrancecazy@MacBook-Pro.lan>
2021-03-23 14:06:40 -0500

    change from food_id to meal_id

--- db/edit_food.sql
+++ db/edit_food.sql
@@ -1,4 +1,4 @@
 UPDATE foods
 SET (title) = ($2)
-WHERE food_id = $1;
+WHERE meal_id = $1;
 SELECT * FROM foods;
--- db/get_food.sql
+++ db/get_food.sql
@@ -1,1 +1,1 @@
-SELECT * FROM foods WHERE food_id = $1;
+SELECT * FROM foods WHERE meal_id = $1;
