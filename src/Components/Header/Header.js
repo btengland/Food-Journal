d8e95c8c035e00db656dabd9ea7252c3f23c56fc
Brady England <bradyeng22@gmail.com>
2021-03-24 15:04:23 -0600

    headerstyled

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,26 @@
 import '../../reset.css'
+import './Header.css'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <div>The Header</div>
+        <div className='header-outer'>
+            <div className='header-inner'>
+                <div>
+                    <Link to='/graphs'>
+                        <h2 className='link'>Graph</h2>
+                    </Link>
+                </div>
+                <div>
+                    <Link to='/main'>
+                        <h2 className='link'>Journal</h2>
+                    </Link>
+                </div>
+                <div>
+                    <button className='logoutbtn'>Logout</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import {Switch, Route} from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import Landing from './Components/Auth/landing'
 import Main from './Components/Main/Main'
+import Graphs from './Components/Graphs/Graphs'
 
 export default (
     <Switch>
-        <Route exact path='/' component={Landing}/>
-        <Route path='/main' component={Main}/>
+        <Route exact path='/' component={Landing} />
+        <Route path='/main' component={Main} />
+        <Route path='/graphs' component={Graphs} />
     </Switch>
 )
