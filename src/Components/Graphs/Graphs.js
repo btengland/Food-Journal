c6bb0905ac2b643905fb584d8ed882d162cfc2fd
Blake Emerson <Blake.c.emerson@gmail.com>
2021-03-29 10:26:49 -0600

    "

--- src/Components/Graphs/Graphs.js
+++ src/Components/Graphs/Graphs.js
@@ -7,3 +7,27 @@ const Graphs = () => {
 }
 
 export default Graphs
+
+// const allergens = data.reduce((acc, mealObj) => {
+// 	const invalidChars = ['\'', '"', '{', '}']
+
+// 	if (mealObj.allergens) {
+// 		const allergensArr = mealObj.allergens.split(',')
+		
+// 		for (i = 0; i < allergensArr.length; i++) {
+// 			for (let j = 0; j < allergensArr[i].length; j++) {
+			
+// 				if (invalidChars.includes(allergensArr[i][j])) {
+// 					allergensArr[i] = allergensArr[i].replace(allergensArr[i][j], '')
+// 					i--;
+// 					break;
+// 				}
+// 			}
+// 		}
+// 		return [...acc, ...allergensArr]
+// 	} else {
+// 		return acc;
+// 	}
+// }, []) 
+
+// console.log(allergens)
