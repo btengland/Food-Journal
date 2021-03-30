7c6b0c242b24c265801b15e7a01f8a98642bccd3
Chris Aragon <caragon.tpp@gmail.com>
2021-03-30 11:57:01 -0500

    grapph debug and box shadow

--- src/Components/Graphs/Linegraph.js
+++ src/Components/Graphs/Linegraph.js
@@ -1,32 +1,12 @@
 import '../../reset.css'
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Line } from 'react-chartjs-2'
 import { connect } from 'react-redux'
 
 const Linegraph = (props) => {
-    const [graphData, setGraphData] = useState([])
+   
 
-
-    const ratings = props.meals.meals.map(i => i.rating)
-    const dates = props.meals.meals.map(i => i._date.substring(0, 10))
-
-    useEffect(() => {
-        setGraphData({
-            labels: dates,
-            datasets: [
-                {
-                    label: 'Feeling',
-                    fill: false,
-                    lineTension: 0.0,
-                    backgroundColor: '#3e7e',
-                    borderColor: 'RGB(0,0,0,1)',
-                    borderWidth: 2,
-                    data: ratings
-                }
-            ]
-        })
-
-    }, [dates, ratings])
+    
 
 //this gives an infinite loop of errors ^^
 
@@ -34,7 +14,20 @@ const Linegraph = (props) => {
     return (
         <div>
             <Line
-                data={graphData}
+                data={{
+                    labels: props.meals.meals.map(i => i._date.substring(0, 10)),
+                    datasets: [
+                        {
+                            label: 'Feeling',
+                            fill: false,
+                            lineTension: 0.0,
+                            backgroundColor: '#3e7e',
+                            borderColor: 'RGB(0,0,0,1)',
+                            borderWidth: 2,
+                            data: props.meals.meals.map(i => i.rating)
+                        }
+                    ]
+                }}
                 height={20}
                 width={100}
                 options={{
